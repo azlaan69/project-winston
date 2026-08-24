444e906e2433177983abf3536cd466c8079c5bf5
azlaan. <azlaannaseem@gmail.com>
2026-08-25 00:22:38 +0500

    god help me is all im saying

--- proto_controller.gd
+++ proto_controller.gd
@@ -1,8 +1,3 @@
-# ProtoController v1.0 by Brackeys
-# CC0 License
-# Intended for rapid prototyping of first-person games.
-# Happy prototyping!
-
 extends CharacterBody3D
 
 @export var can_move : bool = true
@@ -14,7 +9,7 @@ extends CharacterBody3D
 @export_group("Speeds")
 @export var look_speed : float = 0.002
 @export var base_speed : float = 7.0
-@export var jump_velocity : float = 6.0
+@export var jump_speed : float = 6.0
 @export var dash_speed : float = 20.0
 @export var freefly_speed : float = 25.0
 @export var slide_speed : float = 10.0
@@ -36,6 +31,12 @@ var freeflying : bool = false
 var wall_running : bool = false
 var near_wall : bool = false
 var decel_locked : bool = false
+
+var move_velocity: Vector3
+var jump_velocity: Vector3
+var slide_velocity: Vector3
+var dash_velocity: Vector3
+var external_velocity: Vector3
 
 var input_dir = 0.0
 var move_dir = 0.0
@@ -84,7 +85,12 @@ func _unhandled_input(event: InputEvent) -> void:
 			disable_freefly()
 
 func _physics_process(delta: float) -> void:
-	debug.text = str(juice.shift_allowed)
+	debug.text = """Raw Speed: %.1f
+Vel: %s
+Slide: %s
+Dash: %s
+Jump: %s
+Pos: %s""" % [round(velocity.length()), round(velocity), round(slide_velocity), round(dash_velocity), round(jump_velocity), round(global_position)]
 	
 	# If freeflying, handle freefly and nothing else
 	if can_freefly and freeflying:
@@ -96,60 +102,67 @@ func _physics_process(delta: float) -> void:
 	
 	input_dir = Input.get_vector(input_left, input_right, input_forward, input_back)
 	move_dir = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	movestuff(delta)
+	dash(delta)
+	slide(delta)
+	vertical(delta)
 	
+	velocity = move_velocity + jump_velocity + dash_velocity + slide_velocity + external_velocity
 	near_wall = (is_on_wall() and not is_on_floor())
 	
-	if can_dash and Input.is_action_just_pressed(input_dash) and dash_charges > 0:
-		dash_timer.start()
-		dash_charges -= 1
-	
-	else:
-		move_speed = lerp(move_speed, base_speed, 0.75)
+	#if can_dash and Input.is_action_just_pressed(input_dash) and dash_charges > 0:
+		#dash_timer.start()
+		#dash_charges -= 1
+	#
+	#else:
+		#move_speed = lerp(move_speed, base_speed, 0.75)
+		#
+	#if dash_timer.time_left > 0: 
+		#move_speed += dash_speed
+		#velocity.y = 0
+
+	#
+	#if has_gravity and not is_on_floor():
+		#if near_wall:
+			#velocity.y = -0.5
+			#move_speed *= 1.5
+		#else:
+			#velocity += get_gravity() * delta
+#
+	#if can_jump:
+		#if Input.is_action_just_pressed(input_jump):
+			#
+			#if near_wall:
+				#if wall_running: wall_running = false
+				#
+				#var wall_normal = get_wall_normal()
+				#velocity.y += jump_velocity * 1.5
+				#
+				#var jump_dir = (wall_normal * 1.0 + (-transform.basis.z)).normalized()
+				#move_speed = base_speed
+				#velocity.x = jump_dir.x * move_speed
+				#velocity.z = jump_dir.z * move_speed
+				#lock.start()
+				#
+			#elif is_on_floor():
+				#
+				#if dash_timer.time_left > 0.0:
+					#velocity.y = jump_velocity
+					#velocity *= 2.0
+					#lock.start()
+					 #
+				#else:
+					#velocity.y = jump_velocity
+#
+	#if Input.is_action_pressed(input_slide) and is_on_floor():
+		#juice.shift(0.7, 100, 0.0, true)
+		#if move_dir:
+			#velocity += -transform.basis.z * 10.0 * delta
+			#lock.start()
+	#if Input.is_action_just_released(input_slide):
+		#juice.shift(1.7, 90, 0.0, true)
 		
-	if dash_timer.time_left > 0: 
-		move_speed += dash_speed
-		velocity.y = 0
-	if dash_charges < 3 and dash_cd.is_stopped(): dash_cd.start()
-	
-	if has_gravity and not is_on_floor():
-		if near_wall:
-			velocity.y = -0.5
-			move_speed *= 1.5
-		else:
-			velocity += get_gravity() * delta
-
-	if can_jump:
-		if Input.is_action_just_pressed(input_jump):
-			
-			if near_wall:
-				if wall_running: wall_running = false
-				
-				var wall_normal = get_wall_normal()
-				velocity.y += jump_velocity * 1.5
-				
-				var jump_dir = (wall_normal * 1.0 + (-transform.basis.z)).normalized()
-				move_speed = base_speed
-				velocity.x = jump_dir.x * move_speed
-				velocity.z = jump_dir.z * move_speed
-				lock.start()
-				
-			elif is_on_floor():
-				
-				if dash_timer.time_left > 0.0:
-					velocity.y = jump_velocity
-					velocity *= 2.0
-					lock.start()
-					 
-				else:
-					velocity.y = jump_velocity
-
-	if Input.is_action_pressed(input_slide) and is_on_floor():
-		juice.shift(0.7, 100, 0.0, true)
-		if move_dir:
-			velocity += -transform.basis.z * 10.0 * delta
-			lock.start()
-	if Input.is_action_just_released(input_slide):
-		juice.shift(1.7, 90, 0.0, true)
+		
 	#if not is_on_floor() and lock.time_left <= 0.0:
 		#
 		#var wall_normal = get_wall_normal()
@@ -167,15 +180,15 @@ func _physics_process(delta: float) -> void:
 			#velocity.z = (parallel.z * move_speed) + stick_force.z
 			#wall_running = true
 		
-	if can_move and lock.time_left <= 0.0:
-		
-		if move_dir:
-			velocity.x = move_dir.x * move_speed
-			velocity.z = move_dir.z * move_speed
-			
-		elif not decel_locked:
-			velocity.x = lerp(velocity.x, 0.0, 0.2)
-			velocity.z = lerp(velocity.z, 0.0, 0.2)
+	#if can_move and lock.time_left <= 0.0:
+		#
+		#if move_dir:
+			#velocity.x = move_dir.x * move_speed
+			#velocity.z = move_dir.z * move_speed
+			#
+		#elif not decel_locked:
+			#velocity.x = lerp(velocity.x, 0.0, 0.2)
+			#velocity.z = lerp(velocity.z, 0.0, 0.2)
 	
 	if Input.is_action_just_pressed("r") and hat.can_use:
 		hatstuff(delta)
@@ -218,6 +231,38 @@ func release_mouse():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	mouse_captured = false
 
+func movestuff(delta) -> void:
+	if move_dir:
+		move_velocity = move_velocity.move_toward(move_dir * base_speed, 50.0 * delta)
+	else:
+		move_velocity = move_velocity.move_toward(Vector3.ZERO, 30.0 * delta)
+		if move_velocity.length_squared() < 0.01: move_velocity = Vector3.ZERO
+
+func dash(delta) -> void:
+	if dash_charges < 3 and dash_cd.is_stopped(): dash_cd.start()
+	if Input.is_action_just_pressed(input_dash) and dash_charges > 0:
+		jump_velocity = Vector3.ZERO
+		dash_velocity = (dash_velocity + move_dir * 50.0).limit_length(60.0)
+		dash_timer.start()
+		dash_charges -= 1
+	dash_velocity = dash_velocity.move_toward(Vector3.ZERO, 150.0 * delta)
+
+func slide(delta) -> void:
+	if Input.is_action_just_pressed(input_slide) and is_on_floor():
+		slide_velocity = transform.basis.z * 5.0
+	else:
+		slide_velocity = slide_velocity.move_toward(Vector3.ZERO, 10.0 * delta)
+
+func vertical(delta) -> void:
+	if Input.is_action_just_pressed(input_jump) and is_on_floor():
+		jump_velocity.y = 9
+	if not is_on_floor():
+		jump_velocity += get_gravity() * delta
+	if is_on_floor() and jump_velocity.y < 0:
+		jump_velocity = jump_velocity.move_toward(Vector3.ZERO, 100 * delta)
+	
+	if Input.is_action_pressed(input_slide) and is_on_floor():
+		jump_velocity = -transform.basis.z * 5.0
 
 func hatstuff(delta) -> void:
 	match hat.current_state:
