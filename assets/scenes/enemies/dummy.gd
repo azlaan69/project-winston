912d23a31b93b10be1a4220d1acf959660f5a8ad
azlaan. <azlaannaseem@gmail.com>
2026-09-02 23:20:49 +0500

    yes

--- assets/scenes/enemies/dummy.gd
+++ assets/scenes/enemies/dummy.gd
@@ -1,19 +1,23 @@
 extends BaseEnemy
 
 var telegraph_timer: float
+var cooldown: float
 var movement: Vector3
 const BULLET = preload("res://assets/scenes/enemies/bullet1.tscn")
 
 func _physics_process(delta: float) -> void:
 	super(delta)
 	update_nav_target()
+	
 	distance = (player.global_position - global_position)
 		
-	#dir = distance.normalized()
+	dir = distance.normalized()
+	
+	if cooldown >= 0.0: cooldown -= delta
 	
 	if distance.length() <= 50.0 and current_state == state.IDLE: current_state = state.CHASE
 	elif distance.length() >= 70.0 and current_state == state.CHASE: current_state = state.IDLE
-	elif distance.length() <= socialdistance and current_state == state.CHASE: 
+	elif distance.length() <= socialdistance and current_state == state.CHASE and cooldown <= 0.0: 
 		current_state = state.TELEGRAPH
 		telegraph_timer = 0.5
 	
@@ -24,7 +28,8 @@ func _physics_process(delta: float) -> void:
 			var path_dir = get_next_path_dir(delta)
 			var has_path = path_dir != Vector3.ZERO
 			rotate_towards(path_dir, look_speed, delta)
-			var target = -transform.basis.z * speed if has_path else Vector3.ZERO
+			var move_speed = speed if cooldown < 0.0 else speed / 2.0
+			var target = -transform.basis.z * move_speed if has_path else Vector3.ZERO
 			movement = movement.lerp(target, accel * delta)
 		state.TELEGRAPH:
 			var path_dir = get_next_path_dir(delta)
@@ -36,12 +41,12 @@ func _physics_process(delta: float) -> void:
 				movement = Vector3.ZERO
 				current_state = state.ATTACK
 		state.ATTACK:
-			print("pew!")
 			var bullet = BULLET.instantiate()
 			get_parent().add_child(bullet)
 			bullet.global_transform.basis = global_transform.basis
 			bullet.global_position = global_position
-			current_state = state.COOLDOWN
+			cooldown = 5.0
+			current_state = state.IDLE
 	
 	velocity.x = movement.x
 	velocity.z = movement.z
--- assets/scenes/player/bullet1.gd
+++ assets/scenes/player/bullet1.gd
@@ -1,7 +1,7 @@
 extends Area3D
 
 @export var speed: float = 30.0
-@export var lifetime: float = 20.0
+@export var lifetime: float = 5.0
 @export var hit_data: Dictionary = {
 	"damage": 10.0,
 	"knockback": Vector3(1, 0, 1)
@@ -11,8 +11,10 @@ func _physics_process(delta: float) -> void:
 	lifetime -= delta
 	if lifetime <= 0.0: queue_free()
 	global_position += -transform.basis.z * speed * delta
-	$MeshInstance3D.rotate(Vector3.LEFT, 0.01)
+	$MeshInstance3D.rotation.z += 45
 
 func _on_body_entered(body: Node3D) -> void:
+	if body.is_in_group("enemy"): return
 	if body.is_in_group("player"):
 		body.hit(hit_data)
+	queue_free()
